64ed2c1f58527c0ca3d30fe87a3da8706b805517
Slobodianiuk Danylo <lloll1945@gmail.com>
2020-09-20 23:33:28 +0300

    Добавьте файлы проекта.

---
+++ Task_3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Task_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool flag = true;
+            while (flag == true)
+            { 
+                try
+                {
+                    flag = false;
+                    Console.WriteLine("Enter a: ");
+                    int a = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Enter b: ");
+                    double b = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("Enter c: ");
+                    long c = Convert.ToInt64(Console.ReadLine());
+
+                    Console.WriteLine("a = {0}; b = {1}; c = {2}", a, b, c);
+                }
+                catch
+                {
+                    Console.WriteLine("Enter correct value!");
+                    flag = true;
+                }
+            }
+        }
+    }
+}
